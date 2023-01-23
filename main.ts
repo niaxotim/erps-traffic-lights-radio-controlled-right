fa8fd8d3f4f0a85ec82d74facaf2f416599de2b6
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-23 19:27:57 +0000

    Block adjusment

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Start Sequence") {
+        Start_Lights = true
+    }
+})
+let Start_Lights = false
+radio.setGroup(1)
+Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Stop)
 basic.forever(function () {
-	
+    if (Start_Lights == true) {
+        basic.pause(2000)
+        Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.GetReady)
+        basic.pause(500)
+        Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Go)
+        basic.pause(2000)
+        Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.ReadyToStop)
+        basic.pause(1000)
+        Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Stop)
+        Start_Lights = false
+        radio.sendString("Start Sequence")
+    }
 })
